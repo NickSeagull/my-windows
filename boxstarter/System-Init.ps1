5b3eb012cefce46fdfce3afe5f70b0545bc3c0a9
Nikita Tchayka <nikitatchayka@gmail.com>
2019-12-16 16:06:09 +0000

    update

--- boxstarter/System-Init.ps1
+++ boxstarter/System-Init.ps1
@@ -74,6 +74,7 @@ cinst diffutils  # required by emacs
 cinst peazip
 cinst FiraCode
 cinst vcxsrv
+cinst 
 
 ############################################
 #                                          #

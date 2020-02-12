db19737b806901a7b19143314c320282e6d0d254
Nikita Tchayka <nick@booster.cloud>
2020-02-12 19:23:50 +0000

    update

--- autohotkey/Layers.hs
+++ autohotkey/Layers.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main where
 
 import Data.String.Interpolate (i)
@@ -7,15 +9,24 @@ main :: IO ()
 main =
   render
     []
-    [ Layer
+    [ Layer A (remapTo "Shift") "" [],
+      Layer S (remapTo "Shift") "" [],
+      Layer
         { key = W,
           onHold = none,
           description = "General apps",
           rules =
-            [ (V, activate "C:\\Program Files\\Microsoft VS Code\\Code.exe" "Visual Studio Code")
+            [ ( V,
+                activate
+                  "C:\\Program Files\\Microsoft VS Code\\Code.exe"
+                  "Visual Studio Code"
+              )
             ]
         }
     ]
 
 activate :: String -> String -> RuleAction
 activate path title = RuleAction [i|RunOrActivateTitle("#{path}", "#{title}")|]
+
+remapTo :: String -> RuleAction
+remapTo keyString = RuleAction [i|Send % "{#{keyString} " (state? "down" : "up") "}"|]

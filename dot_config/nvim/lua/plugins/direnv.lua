df4046cf6db64a49add767b570334c145e0e8cac
Nikita Tchayka <github@nickseagull.dev>
2024-11-27 09:18:22 +0000

    update

--- dot_config/nvim/lua/plugins/direnv.lua
+++
@@ -1,9 +0,0 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
-
--- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
---
--- In your plugin files, you can:
--- * add extra plugins
--- * disable/enabled LazyVim plugins
--- * override the configuration of LazyVim plugins
-return { "actionshrimp/direnv.nvim", opts = {} }
